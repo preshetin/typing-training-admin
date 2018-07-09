5330b5c8e5b3922327f5616647f2055254aa6664
Petr Reshetin <preshetin@gmail.com>
2018-07-09 20:46:12 +0300

    Filter exercises by lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 import loopbackRestClient from 'aor-loopback';
-import { ExersciseList, ExerciseEdit, ExerciseCreate } from './exercises';
 import { LessonList, LessonEdit, LessonCreate } from './lessons';
+import { ExersciseList, ExerciseEdit, ExerciseCreate } from './exercises';
 
 const App = () => (
   <Admin restClient={loopbackRestClient('http://localhost:3000/api')}>
-      <Resource name="Lessons" list={LessonList} edit={LessonEdit} create={LessonCreate} remove={Delete} />
+      <Resource name="Lessons" list={LessonList}  edit={LessonEdit} create={LessonCreate} remove={Delete} />
       <Resource name="Exercises" list={ExersciseList} edit={ExerciseEdit} create={ExerciseCreate} remove={Delete} />
   </Admin>
 )
--- src/exercises.js
+++ src/exercises.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { List, Datagrid, TextField, EditButton, EditField, ReferenceField, ReferenceInput, DisabledInput, Edit, SimpleForm, required, SelectInput, TextInput, LongTextInput, Create  } from 'admin-on-rest';
+import { List, Filter, Datagrid, TextField, EditButton, EditField, ReferenceField, ReferenceInput, NumberField, DisabledInput, Edit, SimpleForm, required, SelectInput, TextInput, LongTextInput, Create  } from 'admin-on-rest';
+import "./Exercises.css";
+
+const ExerciseFilter = (props) => (
+    <Filter {...props}>
+        <ReferenceInput label="Lesson" source="lessonId" reference="Lessons" alwaysOn>
+            <SelectInput optionText="title" />
+        </ReferenceInput>
+    </Filter>
+);
 
 export const ExersciseList = (props) => (
-    <List {...props}>
+    <List {...props} filters={<ExerciseFilter />} >
         <Datagrid>
             <TextField source="id" />
             <TextField source="title" />
+            <NumberField source="sort" />
             <ReferenceField label="Lesson" source="lessonId" reference="Lessons">
                 <TextField source="title" />
             </ReferenceField>
@@ -19,11 +29,12 @@ export const ExerciseEdit = (props) => (
     <Edit {...props}>
         <SimpleForm>
             <DisabledInput source="id" />
+            <SelectInput source="sort" choices={Array.from(Array(50).keys()).map((_, index) => ({ id: index + 1, name: index + 1 }))} allowEmpty />
             <ReferenceInput label="Lesson" source="lessonId" reference="Lessons" >
                 <SelectInput optionText="title" />
             </ReferenceInput>
             <TextInput source="title" validate={required} />
-            <LongTextInput source="task" validate={required} />
+            <LongTextInput source="task" validate={required}  inputProps={{ spellcheck: false }} />
         </SimpleForm>
     </Edit>
 );
@@ -31,6 +42,7 @@ export const ExerciseEdit = (props) => (
 export const ExerciseCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
+            <SelectInput source="sort" choices={Array.from(Array(50).keys()).map((_, index) => ({ id: index + 1, name: index + 1 }))} allowEmpty />
             <ReferenceInput label="Lesson" source="lessonId" reference="Lessons" allowEmpty>
                 <SelectInput optionText="title" />
             </ReferenceInput>
